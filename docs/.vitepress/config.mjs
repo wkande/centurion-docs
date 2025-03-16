d9a1399bfcd7d1688a947ab50e8a5e458be4cf2b
Warren Anderson <warren@api3.org>
2025-03-16 12:52:44 -0600

    update actions file

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Centurion Documentation",
   description: "A VitePress Site",
-  base: '/repo/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
