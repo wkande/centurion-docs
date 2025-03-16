da94d4e794ceb0aae74d3b48d234472e1160dafe
Warren Anderson <warren@api3.org>
2025-03-16 12:33:29 -0600

    base: '/repo/', per vitepress docs for gitpages

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Centurion Documentation",
   description: "A VitePress Site",
+  base: '/repo/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
